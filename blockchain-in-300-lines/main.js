3bf704e4f98a7f2dac28f8ab2949b72c2909960b
liangpeili <liangpeili@foxmail.com>
2019-10-31 08:18:02 +0800

    添加挖矿功能

--- blockchain-in-300-lines/main.js
+++ blockchain-in-300-lines/main.js
@@ -8,6 +8,7 @@ class Block {
     this.transactions = [];
     this.previousHash = '';
     this.hash = this.calculateHash();
+    this.nonce = 0;
   }
   // 计算区块的哈希值
   calculateHash() {
@@ -25,11 +26,22 @@ class Block {
   getTransactions() {
     return this.transactions;
   }
+
+  // 挖矿
+  mineBlock(difficulty) {
+    console.log(`Mining block ${this.index}`);
+    while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+    console.log("BLOCK MINED: " + this.hash);
+  }
 }
 
 class Blockchain {
   constructor() {
     this.chain = [this.createGenesisBlock()];
+    this.difficulty = 2;
   }
   // 创建创始区块
   createGenesisBlock() {
@@ -45,7 +57,7 @@ class Blockchain {
   // 添加区块到区块链
   addBlock(newBlock) {
     newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.hash = newBlock.calculateHash();
+    newBlock.mineBlock(this.difficulty);
     this.chain.push(newBlock);
   }
   // 验证当前区块链是否有效
@@ -68,28 +80,36 @@ class Blockchain {
   }
 }
 
-// 区块链示例
+// // 区块链示例
+// const testCoin = new Blockchain();
+// console.log(JSON.stringify(testCoin.chain, undefined, 2));
+
+// let block1 = new Block('1', '02/10/2017');
+// block1.addNewTransaction('Alice', 'Bob', 500);
+// testCoin.addBlock(block1);
+
+// let block2 = new Block('2', '03/10/2017');
+// block2.addNewTransaction('Jack', 'David', 1000);
+// testCoin.addBlock(block2);
+
+// console.log(JSON.stringify(testCoin.chain, undefined, 2));
+// console.log(testCoin.isChainValid())
+
+// // 验证防篡改特性
+// // 1. 修改交易
+// block1.transactions[0].amount = 100;
+// console.log(block1.getTransactions())
+// console.log(testCoin.isChainValid())
+
+// // 2.修改交易&修改区块哈希
+// block1.transactions[0].amount = 100;
+// block1.hash = block1.calculateHash();
+// console.log(testCoin.isChainValid())
+
 const testCoin = new Blockchain();
-console.log(JSON.stringify(testCoin.chain, undefined, 2));
 
 let block1 = new Block('1', '02/10/2017');
 block1.addNewTransaction('Alice', 'Bob', 500);
 testCoin.addBlock(block1);
 
-let block2 = new Block('2', '03/10/2017');
-block2.addNewTransaction('Jack', 'David', 1000);
-testCoin.addBlock(block2);
-
-console.log(JSON.stringify(testCoin.chain, undefined, 2));
-console.log(testCoin.isChainValid())
-
-// 验证防篡改特性
-// 1. 修改交易
-block1.transactions[0].amount = 100;
-console.log(block1.getTransactions())
-console.log(testCoin.isChainValid())
-
-// 2.修改交易&修改区块哈希
-block1.transactions[0].amount = 100;
-block1.hash = block1.calculateHash();
-console.log(testCoin.isChainValid())
+console.log(block1)
